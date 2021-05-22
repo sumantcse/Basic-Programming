c73807df876ddcaf2641062c7892d72ab6b63f9d
sumant pandey <sumantcse17@gmail.com>
2021-05-22 13:05:11 +0530

    Added CPP folder with a program

---
+++ c++/function.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace  std;
+	 int addition (int a , int b)
+	 {
+	 	int p;
+	 	p = a + b;
+	 	return (p);
+	 }
+	int main(){
+		int sum;
+		sum = addition (10 , 20);
+		cout << "the result is : " << sum ;
+	}
+	
